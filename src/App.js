ab4fbb7c0af7aa91069703fbaed10610879e2535
kukumbala <televna@gmail.com>
2022-10-25 15:10:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <a href="/" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
+}
