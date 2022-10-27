26b2bb8a739cc3b5894cc791617ce2ebe7d6ed03
kukumbala <televna@gmail.com>
2022-10-27 10:46:45 +0300

    Add search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <a href="/" className="btn btn-primary">
-        Hello
-      </a>
+      <div className="container">
+        <Dictionary />
+      </div>
+      <footer>
+        This project was coded by Alex and is{" "}
+        <a href="https://github.com/kukumbala/dictionary-app">
+          open-sourced by GitHub
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [word, setWord] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for "${word}"`);
+  }
+  function getWord(event) {
+    setWord(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={getWord} />
+      </form>
+    </div>
+  );
+}
