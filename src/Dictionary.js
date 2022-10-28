c9e42c4828e5286376a796d2660ec7dc23b892a0
kukumbala <televna@gmail.com>
2022-10-28 10:09:47 +0300

    Output result

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
+    //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -20,6 +23,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={getWord} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
