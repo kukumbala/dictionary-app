0c6b30ea584ad98b763fe8894f1ec61d4664ffdb
kukumbala <televna@gmail.com>
2022-11-02 15:23:59 +0200

    Add sound

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,18 +1,25 @@
 import React from "react";
+import useSound from "use-sound";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import "./Phonetics.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetics">
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        title="audio"
-      >
-        Listen
-      </a>{" "}
-      {props.phonetic.text}
-    </div>
-  );
+  let [play] = useSound(props.phonetic.audio);
+  if (props.phonetic.audio === "") {
+    return null;
+  } else {
+    if (props.phonetic.text) {
+      return (
+        <div className="Phonetics">
+          <button onClick={play}>
+            <FontAwesomeIcon icon={faCirclePlay} />
+          </button>{" "}
+          {props.phonetic.text}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
